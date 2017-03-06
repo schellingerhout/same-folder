6a7943a1c543d0eb831bf92d298c214f0b77594a
Jasper Schellingerhout <schellingerhout@live.com>
2017-03-05 20:40:46 -0500

    Added note on platforms

--- testsamepath.js
+++ testsamepath.js
@@ -1,1 +1,2 @@
-//since test cases are platform specific testing is a bit of a dilemma
+//since test cases are platform specific testing is a bit of a dilemma.
+//Tests need to be able to determine the file system before being run
